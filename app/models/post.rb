7ab142362875557ae6a693faab07226b2226b607
rcmckee <rmckee8@gmail.com>
2015-03-27 18:41:36 -0500

    improved search dictionary
    so it uses “english” so it will find “character” and “characters” if I
    search for “character”

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search, against: [:title, :link, :details, :institution]
+    using: {tsearch: {dictionary: "english"}}
 
   def self.text_search(query)
     if query.present?
