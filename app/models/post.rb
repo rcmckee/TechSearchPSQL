0f1e3c76c858fc3c0b4fd48dca36d5cea7d27118
rcmckee <rmckee8@gmail.com>
2015-03-30 23:46:29 -0500

    basic search is working with english dictionary
    need to improve the attributes. something about having to convert them
    to strong parameters because this new version doesn’t do the old term
    anymore. But save this for a later time.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
+
   include PgSearch
-  pg_search_scope :search, against: [:title, :link, :details, :institution]
+  pg_search_scope :search, against: [:title, :link, :details, :institution],
     using: {tsearch: {dictionary: "english"}}
+
 
   def self.text_search(query)
     if query.present?
